843a7a3ac3632ed1ea6ee01a5e2b34987c285b96
kudo07 <vknwl077@gmail.com>
2026-06-08 01:34:03 +0530

    feat:module 2

--- module2/cmd/server/main.go
+++ module2/cmd/server/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/kudo07/go-grpc/module2/internal/hello"
 	"github.com/kudo07/go-grpc/module2/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -14,6 +15,7 @@ func main() {
 	helloService := hello.Service{}
 
 	proto.RegisterHelloServiceServer(grpcServer, &helloService)
+	reflection.Register(grpcServer)
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatal(err)
