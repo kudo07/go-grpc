45af4b9e2362c70cddc0652820d474799f4eb6a2
kudo07 <vknwl077@gmail.com>
2026-06-07 23:31:39 +0530

    chore: module 2

---
+++ module2/cmd/server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/kudo07/go-grpc/module2/internal/hello"
+	"github.com/kudo07/go-grpc/module2/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	grpcServer := grpc.NewServer()
+	helloService := hello.Service{}
+
+	proto.RegisterHelloServiceServer(grpcServer, &helloService)
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("starting grpc server on address :%s", ":50051")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
+}
